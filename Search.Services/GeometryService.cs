40eb087711d877644973fb1499ea0f134bc0f66e
Andrew Letskalyuk <andrewletskalyuk@gmail.com>
2024-05-08 21:02:24 +0300

    update logic Search.Service

--- Search.Services/GeometryService.cs
+++ Search.Services/GeometryService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
+using NetTopologySuite.Geometries;
 using Search.Api.Modules.Dtos;
 using Search.Core.Models;
 using Search.Data;
@@ -19,22 +20,38 @@ public class GeometryService : IGeometryService
     }
     public async Task<IEnumerable<Rectangle>> FindIntersectingRectanglesAsync(SegmentDto segment)
     {
+        var factory = new GeometryFactory();
+        var segmentLine = factory.CreateLineString(new[]
+        {
+            new Coordinate(segment.X, segment.Y),
+            new Coordinate(segment.X + segment.Width, segment.Y + segment.Height)
+        });
+
         string cacheKey = $"IntersectingRectangles_{segment.X}_{segment.Y}_{segment.Width}_{segment.Height}";
 
         if (!_cache.TryGetValue(cacheKey, out IEnumerable<Rectangle>? cachedRectangles))
         {
-            cachedRectangles = await _context.Rectangles
-                .Where(r => r.X <= segment.X &&
-                            (r.X + r.Width) >= (segment.X + segment.Width) &&
-                            r.Y <= segment.Y &&
-                            (r.Y + r.Height) >= (segment.Y + segment.Height))
-                .ToArrayAsync();
+            var rectangles = await _context.Rectangles.ToListAsync();
+            var intersectingRectangles = rectangles.Where(r =>
+            {
+                var rectangle = factory.CreatePolygon(new[]
+                {
+                    new Coordinate(r.X, r.Y),
+                    new Coordinate(r.X + r.Width, r.Y),
+                    new Coordinate(r.X + r.Width, r.Y + r.Height),
+                    new Coordinate(r.X, r.Y + r.Height),
+                    new Coordinate(r.X, r.Y)
+                });
+
+                return rectangle.Intersects(segmentLine);
+            }).ToList();
 
             var cacheEntryOptions = new MemoryCacheEntryOptions()
                 .SetSlidingExpiration(TimeSpan.FromMinutes(10)); 
-            //TODO: time must takes from props project
+            // TODO pulling this from config
 
-            _cache.Set(cacheKey, cachedRectangles, cacheEntryOptions);
+            _cache.Set(cacheKey, intersectingRectangles, cacheEntryOptions);
+            cachedRectangles = intersectingRectangles;
         }
 
         return cachedRectangles!;
